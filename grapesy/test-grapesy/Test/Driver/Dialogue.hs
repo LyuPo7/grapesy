12f9364bc117869879aba91ffe5c7ee03c84d097
LyuPo <LyuPo>
2026-03-11 13:33:22 +0300

    [config]: Remove test folders

--- grapesy/test-grapesy/Test/Driver/Dialogue.hs
+++
@@ -1,7 +0,0 @@
-module Test.Driver.Dialogue (
-    module X
-  ) where
-
-import Test.Driver.Dialogue.Definition as X
-import Test.Driver.Dialogue.Execution  as X
-import Test.Driver.Dialogue.Generation as X
